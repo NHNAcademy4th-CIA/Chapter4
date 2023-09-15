1d6f1a29e7c8bed102253948a0e55a8876e2d1a5
minm063 <jangmin063@gmail.com>
2023-09-15 03:11:12 -0700

    docs : quiz

---
+++ src/main/java/org/nhnacademy/minju/Main.java
@@ -0,0 +1,17 @@
+package org.nhnacademy.minju;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+//        logger.info("print capitalized : {}", Exercise1.printCapitalized());
+//        logger.info("hex to decimal : {}", Exercise2.toHex());
+//        logger.info("The number of times you have to roll the dice : {}", Exercise3.targetDiceNumber());
+//        Exercise4.averageNumberOfRolls();
+        Exercise5.checkArrayProcessor();
+//        Exercise8.quiz();
+    }
+}
